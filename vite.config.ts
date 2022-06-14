e5a5150efe4284ed4f69745fc442311556943004
Arnau Espin <43625217+aspnxdd@users.noreply.github.com>
2022-06-15 00:02:33 +0200

    vite build minify to false

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [vue(), Components(), tsconfigPaths()],
 });
